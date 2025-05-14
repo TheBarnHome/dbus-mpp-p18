f8ff32a0462b051c58f9d7417724a0cdd46d7499
TheBarnHome <126877271+TheBarnHome@users.noreply.github.com>
2025-05-14 15:25:14 +0000

    remove log

--- dbus-mppsolar.py
+++ dbus-mppsolar.py
@@ -378,7 +378,6 @@ class DbusMppSolarService(object):
             rated = runInverterCommands('get-rated')
             alerts = runInverterCommands('get-errors')
 
-            logging.warning(rated)
         except:
             logging.warning("Error in update PI18 loop.", exc_info=True)
             self._updateInternal()
